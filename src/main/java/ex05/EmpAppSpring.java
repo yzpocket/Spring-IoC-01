dd9164b0b21281be497b33df712a59855ecea858
INYONG KIM <ohnyong87@gmail.com>
2022-11-23 10:15:53 +0900

    Commit 2
    setter injection
    constructor injection

--- src/main/java/ex05/EmpAppSpring.java
+++ src/main/java/ex05/EmpAppSpring.java
@@ -12,6 +12,12 @@ public class EmpAppSpring {
 		Emp e=ctx.getBean("e3",Emp.class);
 		e.info1();
 		e.info2();
+		System.out.println("---------------");
+		
+		ctx.getBean("e4",Emp.class).info3();
+		System.out.println("---------------");
+		ctx.getBean("e5",Emp.class).info4();
+		
 	}
 
 }
